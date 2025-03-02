5326022cdb66b858ece0fc1b6f739a72bfdaa526
jagadeeshM <mjagadeesh0112@gmail.com>
2025-03-02 14:39:17 +0530

    react

---
+++ ReactJs/app6-intro/src/Jaga.js
@@ -0,0 +1,10 @@
+import Java from "./Java";
+
+function Jaga(){
+    return <div>
+        <h1>welcome to jaga component</h1>
+        <h2>welcome to react js component create Topic !!!</h2>
+        <Java/>
+    </div>
+}
+export default Jaga;
---
+++ ReactJs/app6-intro/src/Jagadeesh.js
@@ -0,0 +1,9 @@
+import Jaga from './Jaga';
+
+function Jagadeesh() {
+    return <div>
+        <h2>welcome to my component !!!</h2>
+        <Jaga/>
+    </div>
+}
+export default Jagadeesh;
