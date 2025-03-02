422422ce422e249052cdaaa491212249db33cd82
jagadeeshM <mjagadeesh0112@gmail.com>
2025-03-02 15:10:20 +0530

    Component creation

--- ReactJs/app6-intro/src/App.js
+++ ReactJs/app6-intro/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Jagadeesh from './Jagadeesh';
+import Comp from './Comp';
 
 function App() {
   return (
     <div className="App">
      <Jagadeesh/>
+
+     <Comp/>
      
     </div>
   );
