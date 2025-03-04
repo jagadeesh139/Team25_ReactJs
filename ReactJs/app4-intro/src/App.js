3ef67beba7291f5016d38d714e093d411c06bf9f
jagadeeshM <mjagadeesh0112@gmail.com>
2025-03-04 13:05:32 +0530

    array and object

--- ReactJs/app4-intro/src/App.js
+++ ReactJs/app4-intro/src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Jagga from './Jagga';
 import Objectt from './Objectt';
+import Chandu from './Chandu';
 
 function App() {
   return (
     <div className="App">
       <Jagga/>
+      <hr/>
+
       <Objectt/>
+      <hr/>
+      <Chandu/>
       
     </div>
   );
---
+++ ReactJs/app4-intro/src/Chandu.jsx
@@ -0,0 +1,43 @@
+import { Component } from "react";
+
+class Chandu extends Component {
+    constructor() {
+        super()
+        this.state = {
+            student: {
+                fname: "jaga",
+                lname: "eesh",
+                email: "Chandu@gmail.com"
+            },
+            person: ["kiran", "hari", "praveen"]
+        }
+    }
+    obt = () => {
+        this.setState({
+            student: {
+                fname: "Chethen",
+                lname: "Chandu",
+                email: "jaggu@gmail.com"
+            }
+
+        })
+    }
+    render() {
+        return <div>
+            <h2>welcome to Object and array Concepts... </h2>
+            <ul>
+                <li>{this.state.student.fname}</li>
+                <li>{this.state.student.lname}</li>
+                <li>{this.state.student.email}</li>
+            </ul>
+            <button onClick={this.obt}>modify Object....</button>
+            <ul>
+                {this.state.person.map(function (val) {
+                    return <li>{val}</li>
+                })
+                }
+            </ul>
+        </div>
+    }
+}
+export default Chandu;
