65c853775611b1840586514c89fae3c0c3217690
jagadeeshM <mjagadeesh0112@gmail.com>
2025-03-04 11:58:24 +0530

    object

---
+++ ReactJs/app4-intro/src/Jagga.jsx
@@ -0,0 +1,38 @@
+import { Component } from "react";
+
+class Jagga extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            student: {
+                fname: "ram",
+                lname: "raju",
+                email: "ram@gmail.com"
+            },
+            person: ["kiran", "kumar", "raju"]
+        }
+    }
+    objeect = () => {
+        this.setState({
+            student: {
+                fname: "raju",
+                lname: "kumar",
+                email: "raju@gmail.com"
+            },
+        })
+    }
+
+    render() {
+        return <div>
+            {/* <h2>{this.state.student.email}</h2> */}
+            <ul>
+                <li>{this.state.student.fname}</li>
+                <li>{this.state.student.lname}</li>
+                <li>{this.state.student.email}</li>
+            </ul>
+            <button onClick={this.objeect}>change Object details</button>
+        </div>
+    }
+}
+export default Jagga;
