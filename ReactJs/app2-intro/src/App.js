2aae3e36a699a34cf77258ee3e4772099ea73cc2
jagadeeshM <mjagadeesh0112@gmail.com>
2025-03-06 11:07:31 +0530

    Component

--- ReactJs/app2-intro/src/App.js
+++ ReactJs/app2-intro/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Jagadeesh from './Jagadeesh';
 
+
 function App() {
   return (
     <div className="App">
       <Jagadeesh/>
+  
       
     </div>
   );
