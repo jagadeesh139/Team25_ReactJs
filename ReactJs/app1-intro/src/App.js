4d1977bffff781f974be3fb2d230d042e290d219
jagadeeshM <mjagadeesh0112@gmail.com>
2025-03-06 16:07:35 +0530

    component CR

--- ReactJs/app1-intro/src/App.js
+++ ReactJs/app1-intro/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import From from './Form';
+import Forms from './Forms';
 
 function App() {
   return (
     <div className="App">
-      <From/>
+      {/* <From/> */}
+      <Forms/>
      
     </div>
   )
--- ReactJs/app1-intro/src/Forms.jsx
+++ ReactJs/app1-intro/src/Forms.jsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 
-class From extends Component {
+class Forms extends Component {
     constructor() {
         super()
         this.state = {
@@ -9,53 +9,47 @@ class From extends Component {
                 lname: "",
                 email: ""
             },
-            user: [] 
+            user: []
         }
     }
-    handlechange = (event) => {
-        console.log(event.target.name);
-        const newperson = { ...this.state.person };
+    handlevent = (event) => {
+        console.log(event.target.name)
+        const newperson = { ...this.state.person }
         newperson[event.target.name] = event.target.value
         this.setState({ person: newperson })
     }
-    addData = () => {
+    updatedata = () => {
         const newUser = [...this.state.user]
         newUser.push(this.state.person)
         this.setState({ user: newUser })
         console.log(this.state.person)
-
     }
+
 
     render() {
         return <div>
             <form action="">
-                <label htmlFor="">First Name:</label>
-                <input type="text" name="fname" id=""
-                    value={this.state.person.fname} onChange={this.handlechange} />{""}
+                <label htmlFor="">Firstname :</label>
+                <input type="text" name="fname" id="" value={this.state.person.fname} onChange={this.handlevent} />{""}
                 <br />
                 <label htmlFor="">LastName :</label>
-                <input type="text" name="lname" id="" value={this.state.person.lname} onChange={this.handlechange} />{""}
+                <input type="text" name="lname" id="" value={this.state.person.lname} onChange={this.handlevent} />{""}
                 <br />
                 <label htmlFor="">Email :</label>
-                <input type="text" name="email" id="" value={this.state.person.email} onChange={this.handlechange} />
-
+                <input type="text" name="email" id="" value={this.state.person.email} onChange={this.handlevent} />{""}
                 <br />
 
-                <button type="button" onClick={this.addData}>Adddata</button>
+                <button onClick={this.updatedata}>updateData</button>
             </form>
             <hr />
-            {this.state.user.map(function (value) {
+            {this.state.user.map(function(value){
                 return <ul>
                     <li>{value.fname}</li>
                     <li>{value.lname}</li>
                     <li>{value.email}</li>
                 </ul>
-
             })}
-
-
-
         </div>
     }
 }
-export default From;
+export default Forms;
