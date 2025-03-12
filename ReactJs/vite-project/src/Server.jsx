2be24e04e50f23512cb4d782083fe209376f0ddb
jagadeeshM <mjagadeesh0112@gmail.com>
2025-03-12 12:56:28 +0530

    crud

--- ReactJs/vite-project/src/Server.jsx
+++ ReactJs/vite-project/src/Server.jsx
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import axios from 'axios'
+
 
 class Server extends Component {
     constructor() {
@@ -15,6 +17,53 @@ class Server extends Component {
             editindex: null
         }
     }
+
+    handelevent = (e) => {
+        const newperson = { ...this.state.person }
+        newperson[e.target.name] = e.target.value
+        this.setState({ person: newperson })
+    }
+    adduser = () => {
+
+
+        // const newuser = [...this.state.user]
+        // newuser.push(this.state.person)
+        // this.setState({ user: newuser })
+        // this.clearuser()
+
+    }
+    handeldelete = (i) => {
+        const newuser = [...this.state.user]
+        newuser.splice(i, 1)
+        this.setState({ user: newuser })
+
+    }
+    handeledit = (value, i) => {
+        { this.setState({ person: value, editindex: null }) }
+
+    }
+    handelupdate = () => {
+        const newuser = [...this.state.user]
+        newuser[this.state.editindex] = this.state.person
+        this.setState({ user: newuser, editindex: null })
+        this.clearuser()
+    }
+
+    clearuser = () => {
+        {
+            this.setState({
+                person: {
+                    fname: "",
+                    lname: "",
+                    email: "",
+                    id: ""
+
+                }
+
+            })
+        }
+    }
+
     render() {
         return <div>
             <form action="">
@@ -27,7 +76,36 @@ class Server extends Component {
                 <input type="text" id="" name="email" value={this.state.person.email} onChange={this.handelevent} />{""}
                 <label htmlFor="">ID</label>
                 <input type="text" id="" name="id" value={this.state.person.id} onChange={this.handelevent} />{""}
+
+                {this.state.editindex === null ? (<button type="button" onClick={this.adduser}>adduser</button>) : (<button type="button" onClick={this.handelupdate}> Update</button>)}
+
+
+
             </form>
+            <table border={1}>
+                <thead>
+                    <tr>
+                        <th>First name</th>
+                        <th>Lastname</th>
+                        <th>email</th>
+                        <th>Id</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.user.map((value, i) => {
+                        return <tr key={i}>
+                            <td>{value.fname}</td>
+                            <td>{value.lname}</td>
+                            <td>{value.email}</td>
+                            <td>{value.id}</td>
+                            <td><button type="button" onClick={() => { this.handeledit(value, i) }}>edit</button></td>
+
+                            <td ><button type="button" onClick={() => { this.handeldelete(i) }}>delete</button></td>
+                        </tr>
+
+                    })}
+                </tbody>
+            </table>
         </div>
     }
 }
