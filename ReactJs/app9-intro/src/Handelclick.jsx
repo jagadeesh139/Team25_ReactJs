7e2a3c652566c99e22c8ec679336ca15f3dbf978
jagadeeshM <mjagadeesh0112@gmail.com>
2025-03-28 16:14:52 +0530

    hoc

--- ReactJs/app9-intro/src/Handelclick.jsx
+++ ReactJs/app9-intro/src/Handelclick.jsx
@@ -3,6 +3,7 @@ import Hoc from "./Hoc";
 
 class Handelclick extends Component {
     render() {
+        
         return <div>
             <h1>{this.props.count}</h1>
             <button onClick={this.props.handelincreament}>increament</button>
