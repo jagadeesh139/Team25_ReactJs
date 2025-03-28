100bfb3a537a8a6480045bedc80bc82ab99fe168
jagadeeshM <mjagadeesh0112@gmail.com>
2025-03-28 16:21:36 +0530

    context api

---
+++ ReactJs/app9-intro/src/Context APi.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+class ContextAPi extends Component {
+    constructor(props) {
+        super(props)
+        this.state={
+            message: "hi welcome to context api...",
+            from:"i am from Context Api"
+        }
+    }
+    render(){
+        return <div>
+
+        </div>
+
+    }
+}
+export default ContextAPi;
