0fa2a8443496a12f827e5572e7d253a3b4aefefe
jagadeeshM <mjagadeesh0112@gmail.com>
2025-04-04 16:35:48 +0530

    hoc

---
+++ ReactJs/app12-intro/src/Click.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Click extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return <div></div>
+    }
+}
+export default Click;
---
+++ ReactJs/app12-intro/src/Hover.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Hover extends Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return <div>
+
+        </div>
+    }
+
+}
+export default Hover;
