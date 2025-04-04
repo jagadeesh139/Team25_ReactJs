1c2cb7ebeb94aefb18ed0fc86ed0b37d18cedf64
jagadeeshM <mjagadeesh0112@gmail.com>
2025-04-04 15:17:52 +0530

    react app

---
+++ ReactJs/app12-intro/src/Hoc.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Hoc extends Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return <div>
+
+        </div>
+    }
+}
+export default Hoc;
