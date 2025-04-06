d30922692a8c858d02a08e867449e8e94ded8a15
jagadeeshM <mjagadeesh0112@gmail.com>
2025-04-06 21:18:02 +0530

    hoc

--- ReactJs/app9-intro/src/Hoc.jsx
+++ ReactJs/app9-intro/src/Hoc.jsx
@@ -5,35 +5,37 @@ const Hoc = (Handelclick) => {
         constructor(props) {
             super(props)
             this.state = {
-                count:1,
+                count: 1,
             }
         }
         handelincreament = () => {
-             this.setState({ count: this.state.count + 5 })
-             }
-        
+            this.setState({ count: this.state.count + 5 })
+        }
+
         handeldecreament = () => {
-            this.setState({ count: this.state.count - 5 }) }
+            this.setState({ count: this.state.count - 5 })
+        }
 
-        
+
         handelreset = () => {
-            this.setState({ count: 1 }) }
+            this.setState({ count: 1 })
+        }
 
 
-        
+
 
         render() {
             return (
-            <Handelclick count={this.state.count}
+                <Handelclick count={this.state.count}
                     handelincreament={this.handelincreament}
                     handeldecreament={this.handeldecreament}
                     handelreset={this.handelreset} />
-                );
+            );
 
-            
+
         }
     }
     return newComponent;
 
-}  
+}
 export default Hoc;
