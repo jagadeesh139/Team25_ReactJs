cf643dd1372365c3f1c0bdceacc34d7c4479bc7e
jagadeeshM <mjagadeesh0112@gmail.com>
2025-04-17 10:22:12 +0530

    redux crud

--- ReactJs/app15-redux-crud/src/Components/Student.jsx
+++ ReactJs/app15-redux-crud/src/Components/Student.jsx
@@ -5,9 +5,37 @@ class Student extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            student: ["kiran", "raju", "ravi"]
+            student: "",
+            editindex: null
         }
     }
+    handelchange = (e) => {
+        this.setState({ student: e.target.value })
+    }
+    handeladd = () => {
+        this.props.dispatch({
+            type: "CREATE",
+            payload: this.state.student
+        })
+        this.setState({ student: "" })
+
+    }
+    handeledit = (student, i) => {
+        this.setState({ student, editindex: i })
+
+    }
+    handelUpdate = () => {
+        this.props.dispatch({
+            type: "Update",
+            payload: {
+                index: this.state.editindex,
+                value: this.state.student
+            }
+
+        })
+
+    }
+
     render() {
         return <div>
             {/* {this.state.student.map((std) => {
@@ -15,7 +43,20 @@ class Student extends Component {
                 
 
             })} */}
-            <input type="text" />
+            <input type="text" value={this.state.student} onChange={this.handelchange} />
+            {this.state.editindex === null ? <button type="button" onClick={this.handeladd}>adduser</button> : <button type="button" onClick={this.handelUpdate}>Update</button>}
+            <ul>
+                {this.state.student.map(() => {
+                    return <li key={i}><span onClick={this.handeledit}>
+                        edit
+                    </span></li>
+                })}
+            </ul>
+            <span onClick={this.handeldelete}>Delete</span>
+
+
+
+
 
         </div>
     }
