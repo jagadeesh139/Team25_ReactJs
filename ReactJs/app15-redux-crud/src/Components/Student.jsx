b3aa01a026cdd7fd0bff672f4775ffabdff6c8da
jagadeeshM <mjagadeesh0112@gmail.com>
2025-04-17 11:13:33 +0530

    redux crud

--- ReactJs/app15-redux-crud/src/Components/Student.jsx
+++ ReactJs/app15-redux-crud/src/Components/Student.jsx
@@ -1,67 +1,82 @@
 import { Component } from "react";
-import { connect } from "react-redux"
+import { connect } from "react-redux";
 
 class Student extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            student: "",
-            editindex: null
-        }
-    }
-    handelchange = (e) => {
-        this.setState({ student: e.target.value })
-    }
-    handeladd = () => {
-        this.props.dispatch({
-            type: "CREATE",
-            payload: this.state.student
-        })
-        this.setState({ student: "" })
+  constructor(props) {
+    super(props);
+    this.state = {
+      student: "",       // single input string
+      editindex: null    // index of the student being edited
+    };
+  }
 
-    }
-    handeledit = (student, i) => {
-        this.setState({ student, editindex: i })
+  handleChange = (e) => {
+    this.setState({ student: e.target.value });
+  };
 
-    }
-    handelUpdate = () => {
-        this.props.dispatch({
-            type: "Update",
-            payload: {
-                index: this.state.editindex,
-                value: this.state.student
-            }
+  handleAdd = () => {
+    this.props.dispatch({
+      type: "CREATE",
+      payload: this.state.student
+    });
+    this.setState({ student: "" });
+  };
 
-        })
+  handleEdit = (student, i) => {
+    this.setState({ student, editindex: i });
+  };
 
-    }
+  handleUpdate = () => {
+    this.props.dispatch({
+      type: "UPDATE",
+      payload: {
+        index: this.state.editindex,
+        value: this.state.student
+      }
+    });
+    this.setState({ student: "", editindex: null });
+  };
 
-    render() {
-        return <div>
-            {/* {this.state.student.map((std) => {
-                return <li>{std}</li>
-                
+  handleDelete = (i) => {
+    this.props.dispatch({
+      type: "DELETE",
+      payload:i
+    });
+  };
 
-            })} */}
-            <input type="text" value={this.state.student} onChange={this.handelchange} />
-            {this.state.editindex === null ? <button type="button" onClick={this.handeladd}>adduser</button> : <button type="button" onClick={this.handelUpdate}>Update</button>}
-            <ul>
-                {this.state.student.map(() => {
-                    return <li key={i}><span onClick={this.handeledit}>
-                        edit
-                    </span></li>
-                })}
-            </ul>
-            <span onClick={this.handeldelete}>Delete</span>
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.student}
+          onChange={this.handleChange}
+        />
+        {this.state.editindex === null ? (
+          <button onClick={this.handleAdd}>Add User</button>
+        ) : (
+          <button onClick={this.handleUpdate}>Update</button>
+        )}
 
-
-
-
-
-        </div>
-    }
+        <ul>
+          {this.props.student &&
+            this.props.student.map((student, i) => (
+              <li key={i}>
+                {student}
+                <button onClick={() => this.handleEdit(student, i)}>Edit</button>
+                <button onClick={() => this.handleDelete(i)}>Delete</button>
+              </li>
+            ))}
+        </ul>
+      </div>
+    );
+  }
 }
-function mapstatetoprops(state) {
-    return state
+
+function mapStateToProps(state) {
+  return {
+    student: state.student // assuming `state.student` is an array
+  };
 }
-export default connect(mapstatetoprops)(Student)
+
+export default connect(mapStateToProps)(Student);
--- ReactJs/app15-redux-crud/src/Store/StudentReducers.js
+++ ReactJs/app15-redux-crud/src/Store/StudentReducers.js
@@ -2,12 +2,17 @@ const student = ["kiran", "ravi", "raju"]
 const StudentReducers = (state = student, action) => {
     switch (action.type) {
         case "CREATE":
+            return [...state, action.payload]
             break;
         case "READ":
             break;
-        case "Update":
+        case "UPDATE":
+            return state.map((student, i) => {
+                i === action.payload.index ? action.payload.value : student
+            })
             break;
-        case "Delete":
+        case "DELETE":
+            return state.filter((student) => (student !== action.payload))
             break;
         default:
             return state;
