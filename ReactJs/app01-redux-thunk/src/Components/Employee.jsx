9c1c4bec331de049b66b7de9ab86aa90da4a2840
jagadeeshM <mjagadeesh0112@gmail.com>
2025-04-18 13:29:03 +0530

    redux thunk

--- ReactJs/app01-redux-thunk/src/Components/Employee.jsx
+++ ReactJs/app01-redux-thunk/src/Components/Employee.jsx
@@ -14,9 +14,70 @@ class Employee extends Component {
             editindex: null
         }
     }
+    handelevent = (e) => {
+        const newEmpdetails = { ...this.state.Empdetails }
+        newEmpdetails[e.target.name] = e.target.value
+        this.setState({ Empdetails: newEmpdetails })
+
+    }
+    handelAdduser = () => {
+
+    }
+
+    handeldelete = () => {
+
+    }
+    handeledit = () => {
+
+    }
+    handelUpdate = () => {
+
+    }
     render() {
-        return
-        
+        return <div>
+            <form action="">
+                <label htmlFor="">First Name:-</label>
+                <input type="text" value={this.state.Empdetails.fname} name="fname" onChange={this.handelevent} />{""}
+                <br />
+                <label htmlFor="">Last Name:-</label>
+                <input type="text" value={this.state.Empdetails.lname} name="lname" onChange={this.handelevent} />{""}
+                <br />
+                <label htmlFor="">Email:-</label>
+                <input type="text" value={this.state.Empdetails.email} name="email" onChange={this.handelevent} />{""}
+                <br />
+                <label htmlFor="">Password:-</label>
+                <input type="text" value={this.state.password} name="password" onChange={this.handelevent} />{""}
+                <br />
+                {this.state.editindex === null ? (<button type="button" onClick={this.handelAdduser}>Adddetails</button>) : (<button type="button" onClick={this.handelUpdate}>Update</button>)}
+            </form>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>PassWord</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.Empdetails && this.state.Empdetails.map((emp, i) => {
+                        return <tr>
+                            <td>{emp.fname}</td>
+                            <td>{emp.lname}</td>
+                            <td>{emp.email}</td>
+                            <td>{emp.password}</td>
+                            <td><button type="button" onClick={() => { this.handeledit(emp, i) }}>Edit</button></td>
+                            <td><button type="button" onClick={() => { this.handeldelete(emp) }}>Delete</button></td>
+                        </tr>
+
+                    })}
+
+                </tbody>
+            </table>
+        </div>
+
     }
 }
 function mapsStatetoProps(state) {
