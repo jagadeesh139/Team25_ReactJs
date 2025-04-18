ea67de3c813bfe7b3b970524de0fe65af720f741
jagadeeshM <mjagadeesh0112@gmail.com>
2025-04-18 14:54:25 +0530

    thunk redux

--- ReactJs/app01-redux-thunk/src/Store/actions.js
+++ ReactJs/app01-redux-thunk/src/Store/actions.js
@@ -2,13 +2,33 @@ import { axios } from "axios"
 
 const addemployeeuser = (emp) => {
     return async (dispatch) => {
-        const resp = await axios.post("http://localhost:3000/emp", emp)
-        const response = await axios.get("http://localhost:3000/emp")
-        dispatch({
-            type="GET-emp",
-            payload: response.data
-        })
+        try {
+            const resp = await axios.post("http://localhost:3000/emp", emp)
+            const response = await axios.get("http://localhost:3000/emp")
+            dispatch({
+                type: "GET-emp",
+                payload: response.data
+            })
+        } catch (error) { }
 
     }
+}
+
+const deleteemployeeuser = (emp) => {
+    return async (dispatch) => {
+        try {
+            const resp = await axios.delete("http://localhost:3000/emp", emp.id)
+            const response = await axios.get("http://localhost:3000/emp")
+            dispatch({
+                type: "Delete-emp",
+                payload: response.data
+            })
+
+        } catch (error) {
+
+        }
+
+    }
+
 }
 
