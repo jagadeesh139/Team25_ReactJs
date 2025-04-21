8c4a04e4840f5ae611258849c56e72cada6764e5
jagadeeshM <mjagadeesh0112@gmail.com>
2025-04-21 14:36:00 +0530

    context api

--- ReactJs/app9-intro/src/ContextDemo.jsx
+++ ReactJs/app9-intro/src/ContextDemo.jsx
@@ -2,4 +2,4 @@
 
 // export const UserContext = UserContext()
 // export const UserContextProvider = UserContext.Provider
-// // export const UserContextComsumer = UserContext.Comsumer
+// export const UserContextComsumer = UserContext.Comsumer
