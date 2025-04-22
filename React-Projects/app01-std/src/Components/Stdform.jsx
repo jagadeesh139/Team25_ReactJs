989f587798473b6a5591d29e26d27b818a9f59cf
jagadeeshM <mjagadeesh0112@gmail.com>
2025-04-22 16:15:07 +0530

    project

---
+++ React-Projects/app01-std/src/Components/Stdform.jsx
@@ -0,0 +1,27 @@
+const Stdform = () => {
+    return <div>
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Student Name</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder" />
+        </div>
+        <br />
+        <div class="mb-3">
+            <label for="formGroupExampleInput2" class="form-label">Student Email</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder" />
+        </div>
+        <br />
+        <label htmlFor="">Courses</label>
+        <input type="checkbox" name="" id="" />Java script <input type="text" />React Js <input type="text" />Html
+        <br />
+        <div class="mb-3">
+            <label for="formGroupExampleInput2" class="form-label">Another label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder" />
+        </div>
+        <br />
+        <div class="mb-3">
+            <label for="formGroupExampleInput2" class="form-label">Another label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder" />
+        </div>
+
+    </div>
+}
