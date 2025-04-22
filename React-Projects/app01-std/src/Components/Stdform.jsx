8d974a2b77d6d1d2ad54077ed4b5f91a9f08f4ec
jagadeeshM <mjagadeesh0112@gmail.com>
2025-04-22 16:25:11 +0530

    project

--- React-Projects/app01-std/src/Components/Stdform.jsx
+++ React-Projects/app01-std/src/Components/Stdform.jsx
@@ -1,27 +1,57 @@
-const Stdform = () => {
-    return <div>
-        <div class="mb-3">
-            <label for="formGroupExampleInput" class="form-label">Student Name</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder" />
-        </div>
-        <br />
-        <div class="mb-3">
-            <label for="formGroupExampleInput2" class="form-label">Student Email</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder" />
-        </div>
-        <br />
-        <label htmlFor="">Courses</label>
-        <input type="checkbox" name="" id="" />Java script <input type="text" />React Js <input type="text" />Html
-        <br />
-        <div class="mb-3">
-            <label for="formGroupExampleInput2" class="form-label">Another label</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder" />
-        </div>
-        <br />
-        <div class="mb-3">
-            <label for="formGroupExampleInput2" class="form-label">Another label</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder" />
-        </div>
+import { Component } from "react";
 
-    </div>
+
+class Stdform extends Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return <div>
+            <div class="form-container">
+                <h2>Student Enrollment Form</h2>
+                <form id="enrollmentForm">
+                    <div class="form-group">
+                        <label for="fullName">Full Name</label>
+                        <input type="text" id="fullName" name="fullName" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="dob">Date of Birth</label>
+                        <input type="date" id="dob" name="dob" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="gender">Gender</label>
+                        <select id="gender" name="gender" required>
+                            <option value="">-- Select --</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="other">Other</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email Address</label>
+                        <input type="email" id="email" name="email" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="course">Course Enrolled</label>
+                        <input type="text" id="course" name="course" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="contact">Contact Number</label>
+                        <input type="tel" id="contact" name="contact" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="gender">Batch</label>
+                        <select id="gender" name="gender" required>
+                            <option value="">-- Select --</option>
+                            <option value="male">BatchA1</option>
+                            <option value="female">BatchA2</option>
+                            <option value="other">BatchA3</option>
+                        </select>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+    }
 }
+export default Stdform;
