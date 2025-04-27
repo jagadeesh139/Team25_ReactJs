a367a0a0d3d03d2b854618ffffcf4ec98a4e3e97
jagadeeshM <mjagadeesh0112@gmail.com>
2025-04-27 23:23:23 +0530

    interview

--- React-Projects/app3-interview/src/Interview.jsx
+++ React-Projects/app3-interview/src/Interview.jsx
@@ -34,3 +34,45 @@ function compose(...fns) {
   // Step by step: add5(5) = 10, double(10) = 20, subtract3(20) = 17
   // Output: 17
   
+
+//   Polyfill for Promise.all
+function promiseAll(promises) {
+    return new Promise((resolve, reject) => {
+      let result = [];
+      let completed = 0;
+  
+      promises.forEach((p, index) => {
+        Promise.resolve(p).then(value => {
+          result[index] = value;
+          completed++;
+  
+          if (completed === promises.length) {
+            resolve(result);
+          }
+        }).catch(reject);
+      });
+    });
+  }
+  
+  // Example
+  promiseAll([Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)])
+    .then(console.log) // [1, 2, 3]
+    .catch(console.error);
+
+    // Polyfill for Promise.race
+
+    function promiseRace(promises) {
+        return new Promise((resolve, reject) => {
+          promises.forEach(p => {
+            Promise.resolve(p).then(resolve).catch(reject);
+          });
+        });
+      }
+      
+      // Example
+      promiseRace([
+        new Promise(res => setTimeout(() => res("First"), 100)),
+        new Promise(res => setTimeout(() => res("Second"), 200))
+      ]).then(console.log); // "First"
+      
+  
