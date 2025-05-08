8c2e19494e6ddad3abad6dbe2f15bf6a5a9628da
jagadeeshM <mjagadeesh0112@gmail.com>
2025-05-08 22:53:57 +0530

    project

--- React-Projects/app1-studentenroll/src/components/StudentForm.tsx
+++ React-Projects/app1-studentenroll/src/components/StudentForm.tsx
@@ -125,7 +125,7 @@ export function StudentForm({ onAddStudent, existingStudents, maxEnrollments, cu
               </FormItem>
             )}
           />
-          
+           
           <FormField
             control={form.control}
             name="email"
