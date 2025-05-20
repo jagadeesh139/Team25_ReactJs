e4502845f6e281375335881885e79c6fa23d83d8
jagadeeshM <mjagadeesh0112@gmail.com>
2025-05-20 22:21:10 +0530

    context api

--- ReactJs/app9-intro/src/Context-Component/ParentComponent.jsx
+++ ReactJs/app9-intro/src/Context-Component/ParentComponent.jsx
@@ -4,13 +4,13 @@ import ChildComponent from "./ChildComponent";
 class ParentComponent extends Component {
     constructor() {
         super()
-    }
+    } 
     render() {
         return <div>
             <h1>welcome to parent Component...!!</h1>
             <ChildComponent msg={this.props.msg} />
 
         </div>
-    }
+    } 
 }
 export default ParentComponent;
