336fa1eccdd248dcbdb590eef1adc5410057fe18
jagadeeshM <mjagadeesh0112@gmail.com>
2025-05-29 20:51:37 +0530

    project

--- React-Projects/app15-todolist/src/List.jsx
+++ React-Projects/app15-todolist/src/List.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 
 const List = ({ filterFunction }) => {
-      useEffect(() => {
+  useEffect(() => {
     console.log("List Component Rendered!");
   });
   return (
-  
+
     <ul>
       {filterFunction().map((item) => (
         <li key={item.id}>{item.name}</li>
