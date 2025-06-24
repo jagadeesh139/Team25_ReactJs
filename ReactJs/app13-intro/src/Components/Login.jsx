56f3f903f6162353fb2f4d24bebcc7f64830bb2a
jagadeeshM <mjagadeesh0112@gmail.com>
2025-06-24 12:13:00 +0530

    practice

--- ReactJs/app13-intro/src/Components/Login.jsx
+++ ReactJs/app13-intro/src/Components/Login.jsx
@@ -14,7 +14,7 @@ class Login extends Component {
             errorMessage: "",
         };
     }
-    
+
     handleDashboardNavigation = () => {
         this.props.navigate("/dashboard");
     };
