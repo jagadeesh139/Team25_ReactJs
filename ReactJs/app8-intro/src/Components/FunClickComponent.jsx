7812b0cb3271de97933941ff4163d47a95db1d50
jagadeeshM <mjagadeesh0112@gmail.com>
2025-06-26 00:06:25 +0530

    prop drill down

--- ReactJs/app8-intro/src/Components/FunClickComponent.jsx
+++ ReactJs/app8-intro/src/Components/FunClickComponent.jsx
@@ -1,11 +1,11 @@
 const FunClickComponent = (
-    
-    {count,increment,decrement,reset}) => {
+
+    { count, increment, decrement, reset }) => {
     return (<div>
         <h2>{count}</h2>
-        <button type="button" onClick={ increment }>Increment</button>
-        <button type="button" onClick= { decrement }>Decrement</button>
-        <button type="button" onClick= { reset }>Reset</button>
+        <button type="button" onClick={increment}>Increment</button>
+        <button type="button" onClick={decrement}>Decrement</button>
+        <button type="button" onClick={reset}>Reset</button>
 
     </div>)
 }
